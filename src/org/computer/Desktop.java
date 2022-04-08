7cc59bb9b5c23e958efa255eb6763236a0ab8ad8
prabhakaran231997 <prabhakarancivil23@gmail.com>
2022-04-08 13:00:28 +0530

    done

---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,13 @@
+package org.computer;
+
+public class Desktop {
+	public void desktopSize() {
+		System.out.println("Desktopsize:32GB");
+	}
+
+	public static void main(String[] args) {
+		Desktop q = new Desktop();
+		q.desktopSize();
+	}
+
+}
