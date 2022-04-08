a49362fac9d87761fc8ddbca037427071606b671
prabhakaran231997 <prabhakarancivil23@gmail.com>
2022-04-08 13:15:59 +0530

    allwin has added

--- src/org/computer/Desktop.java
+++ src/org/computer/Desktop.java
@@ -4,10 +4,14 @@ public class Desktop {
 	public void desktopSize() {
 		System.out.println("Desktopsize:32GB");
 	}
+	public void add22() {
+System.out.println("allwin");
+	}
 
 	public static void main(String[] args) {
 		Desktop q = new Desktop();
 		q.desktopSize();
+		q.add22();
 	}
 
 }
